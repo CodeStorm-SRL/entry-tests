09f50130b27c216f4e4e730671eae3611522348d
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-11 12:06:52 +0100

    Implementa componente-Answer

---
+++ js/react-quiz-app/src/components/molecules/Answer.tsx
@@ -0,0 +1,53 @@
+import { FC, PropsWithChildren } from "react";
+import Text from "../atoms/Text";
+import { useAppSelector } from "../../hooks/reduxHooks";
+
+// Props di answer: selected indica l'id della risposta selezionata
+// (l'indice nell'array di risposte), id indica l'id della risposta attuale
+// submittedAnswer indica la risposta confermata dell'utente
+// onClick è la funzione che permette di selezionare e deselezionare la risposta (vedi custom hook useGameLogic)
+type AnswerProps = PropsWithChildren<{
+  selected: number | null;
+  id: number;
+  submittedAnswer: string;
+  onClick(id: number, text: React.ReactNode): void;
+}>;
+
+const Answer: FC<AnswerProps> = ({
+  selected,
+  id,
+  submittedAnswer,
+  onClick,
+  children,
+}: AnswerProps) => {
+  // Seleziono la risposta corretta della domanda corrente
+  const { arrOfQuestions, index } = useAppSelector((state) => state.quiz);
+  const correctAnswer = arrOfQuestions[index].correctAnswer;
+
+  // Se l'id della risposta selezionata corrisponde con l'id della risposta corrente
+  // cambio lo stile
+  const active = selected === id;
+
+  // Determina se il feedback che riceverà l'utente quando risponde suggerisce una
+  // risposta corretta o un errore
+  const feedback =
+    submittedAnswer === correctAnswer && children === correctAnswer;
+
+  // Se la risposta è stata data ed è corretta: feedback success, altrimenti feedback danger
+  // Se la risposta non è stata data: stile che cambia se l'utente ha selezionato o no una risposta
+  const classNames = submittedAnswer
+    ? `${feedback ? "bg-green-700" : ""} ${
+        !feedback && active ? "bg-red-500" : ""
+      }`
+    : `${active ? "bg-blue-400" : "hover:bg-orange-200"}`;
+
+  return (
+    <div
+      className={`${classNames} p-x3 py-5 cursor-pointer`}
+      onClick={() => onClick(id, children)}
+    >
+      <Text variant="secondary">{children}</Text>
+    </div>
+  );
+};
+export default Answer;
