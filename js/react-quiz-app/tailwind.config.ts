4abcd96a00b0d8ad5a6d2a68cba3563900c31561
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-11 11:21:14 +0100

    installazione librerie: redux/rtk/router/tailwind

---
+++ js/react-quiz-app/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
