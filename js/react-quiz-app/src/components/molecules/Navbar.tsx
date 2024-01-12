dae81940023ea4fe7e2be88b405b2783dc20a95c
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-12 16:07:20 +0100

    Implementa fn-returnToMainPage

--- js/react-quiz-app/src/components/molecules/Navbar.tsx
+++ js/react-quiz-app/src/components/molecules/Navbar.tsx
@@ -1,10 +1,25 @@
 import { FC } from "react";
 import Text from "../atoms/Text";
 
+import useMainNav from "../../hooks/useMainNav";
+
 const Navbar: FC = () => {
+  const { appState, navigate } = useMainNav();
+  const notInMainPage = appState !== "start";
+
+  function backToMainPage(): void {
+    if (notInMainPage) navigate("/");
+  }
+
   return (
     <nav className="px-3 py-5 text-center">
-      <Text as="h1">The Quiz</Text>
+      <Text
+        as="h1"
+        className={notInMainPage ? "cursor-pointer inline-block" : ""}
+        onClick={backToMainPage}
+      >
+        The Quiz
+      </Text>
     </nav>
   );
 };
