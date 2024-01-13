4246e6a6888d50c1a3cd386bda9276b102406c50
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-13 15:11:48 +0100

    Implementa atoms

--- js/react-image-gallery/src/components/layout/AppLayout.tsx
+++ js/react-image-gallery/src/components/layout/AppLayout.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
+import Text from "../atoms/Text";
+import Button from "../atoms/Button";
 
 const AppLayout: FC = () => {
   return (
-    <div className="flex flex-col min-h-[100vh] w-full px-5 pb-3 pt-5 "></div>
+    <div className="flex flex-col min-h-[100vh] w-full px-5 pb-3 pt-5 ">
+      <Text>Ciao</Text>
+      <div>
+        <Button>Ciao</Button>
+      </div>
+    </div>
   );
 };
 export default AppLayout;
