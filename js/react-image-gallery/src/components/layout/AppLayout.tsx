07001bdefe59eddc6e474a21fab48f8854e4df1f
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-13 14:10:03 +0100

    Implementa routing

--- js/react-image-gallery/src/components/layout/AppLayout.tsx
+++ js/react-image-gallery/src/components/layout/AppLayout.tsx
@@ -1,10 +1,8 @@
-import { FC, PropsWithChildren } from "react";
+import { FC } from "react";
 
-const AppLayout: FC<PropsWithChildren> = ({ children }) => {
+const AppLayout: FC = () => {
   return (
-    <div className="flex flex-col min-h-[100vh] w-full px-5 pb-3 pt-5">
-      {children}
-    </div>
+    <div className="flex flex-col min-h-[100vh] w-full px-5 pb-3 pt-5"></div>
   );
 };
 export default AppLayout;
