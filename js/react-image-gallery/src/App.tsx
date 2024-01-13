d8b3a280871cb259c5394dd90fc2641b8d9a0bdd
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-13 13:56:33 +0100

    Inizializzazione progetto

---
+++ js/react-image-gallery/src/App.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
