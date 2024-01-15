a2081fafcaa490294af41cf66ad708f535291de5
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-01-15 13:07:12 +0100

    Implementa update PhotoSection

--- js/react-image-gallery/src/components/molecules/ErrorComponent.tsx
+++ js/react-image-gallery/src/components/molecules/ErrorComponent.tsx
@@ -2,7 +2,7 @@ import Text from "../atoms/Text";
 
 const ErrorComponent = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-full h-full text-dark">
       <Text as="h2">Something goes wrong... 😢</Text>
     </div>
   );
