f9794e3261abc23c6fed258e69db49d4efdb7c44
giov-scar <giovanniscarantino@gmail.com>
2024-01-16 15:27:19 +0100

    setting up project

--- js/my-tests/src/app/app-routing.module.ts
+++ js/my-tests/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: LandingComponent
+  },
+  {
+    path:'gallery',
+    component: GalleryComponent
+  },
+  {
+    path:'quiz',
+    component: QuizComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- js/my-tests/src/app/app.module.ts
+++ js/my-tests/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    QuizComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
