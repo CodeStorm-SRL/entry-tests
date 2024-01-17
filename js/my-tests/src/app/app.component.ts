8a482674fbc2894514caf6a632ee676095d0ff98
giov-scar <giovanniscarantino@gmail.com>
2024-01-17 12:11:22 +0100

    updating version

--- js/my-tests/src/app/app.component.ts
+++ js/my-tests/src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { initFlowbite } from 'flowbite';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'my-tests';
-
-  ngOnInit(): void {
-    initFlowbite()
-  }
-
 }
