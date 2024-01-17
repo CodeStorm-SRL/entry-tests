717926ac539f70c322e7ba35ff004b5c56602564
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-17 17:30:02 +0100

    vuex install + store js

--- dinamic-gallery/src/main.js
+++ dinamic-gallery/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+// Foglio di stile globale
+import "./style.css";
+
+import App from "./App.vue";
+
+// Store js
+import store from "./store.js";
+
+createApp(App).use(store).mount("#app");
