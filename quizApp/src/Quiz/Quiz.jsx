97cbaed8a05baad91ccbfee925c9f021b7de02e9
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2024-01-18 16:44:04 +0100

    addition of styling and css to results

--- quizApp/src/Quiz/Quiz.jsx
+++ quizApp/src/Quiz/Quiz.jsx
@@ -7,6 +7,10 @@ import { useState } from "react";
 //import useRef per tenere traccia delle risposte date
 import { useRef } from "react";
 
+//import le immagini per i risultati
+import passed from "../assets/passed.jpeg"
+import failed from "../assets/failed.jpg"
+
 export default function Quiz() {
   //utilizzo useState per aggiornare dinamicamente l'indice per cambiare le domande
   const [actualIndex, setActualIndex] = useState(0);
@@ -86,70 +90,115 @@ export default function Quiz() {
     <>
       {/* imposto un ternary: se completeState equivale a 30 allora il quiz è terminato ed avremo i risultati. Il ternary ha 3 condizioni: se è finito e l'utente ha fatto più di 3 errori verrà detto lui che è bocciato e potrà ricominciare, se ha fatto meno di 4 errori sarà stato promosso e potrà ricominciare, la terza condizione riconosce che il quiz non è finito e va avanti*/}
       {completeState === 30 && correctAnswers > 26 ? (
-        <>
-          <h1>
-            Quiz completato. Sei passato! Hai fatto {30 - correctAnswers} errori
-          </h1>
-          <button onClick={pageReload}>Ricomincia Quiz</button>
+        <>          <main className="flex m-auto justify-center flex-col p-10 ">
+          <div className="max-w-sm rounded bg-amber-100 overflow-hidden shadow-lg m-auto">
+            <img className="w-full" src={passed} alt="failed" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Test superato.</div>
+              <h1>
+                Quiz completato. Bravissimo! Hai superato il quiz con {30 - correctAnswers} errori.
+
+              </h1>
+
+              <button onClick={pageReload} className=" mt-6 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                Ricomincia Quiz
+              </button>
+
+            </div>
+
+          </div>
+
+        </main>
+
         </>
       ) : completeState === 30 && correctAnswers <= 26 ? (
         <>
-          <h1>
-            Quiz completato. Purtroppo sei bocciato.... Hai fatto{" "}
-            {30 - correctAnswers} errori
-          </h1>
-          <button onClick={pageReload}>Ricomincia Quiz</button>
+          <main className="flex m-auto justify-center flex-col p-10 ">
+            <div className="max-w-sm rounded bg-amber-100 overflow-hidden shadow-lg m-auto">
+              <img className="w-full" src={failed} alt="failed" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">Test non superato</div>
+                <h1>
+                  Quiz completato. Purtroppo non hai superato il test.... Hai fatto{" "}
+                  {30 - correctAnswers} errori
+                </h1>
+
+                <button onClick={pageReload} className=" mt-6 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                  Ricomincia Quiz
+                </button>
+
+              </div>
+
+            </div>
+
+          </main>
         </>
       ) : (
         <>
-          {" "}
-          <h1 className="m-auto mt-4 text-center text-black-500  text-4xl">
+          <h1 className="m-auto p-6 text-center text-black-500  text-4xl">
             Quiz Patente
           </h1>
-          <main className=" flex flex-col m-auto">
-            {quizAnswersQuestions[actualIndex].image != "" ? (
-              <img
-                className="w-40 h-40 mt-10 m-auto "
-                src={quizAnswersQuestions[actualIndex].image}
-                alt="noImage"
-              />
-            ) : (
-              <div className="w-40 h-40 mt-10 m-auto"></div>
-            )}
-            <h2 className=" mt-5 text-center">
-              {quizAnswersQuestions[actualIndex].question}
-            </h2>
-            <ul className="m-auto flex justify-evenly">
-              <li>
-                <button
-                  onClick={(e) => {
-                    check(e, "T");
-                  }}
-                  id="true"
-                  className="bg-blue-500  text-white font-bold py-2 px-4 rounded"
+          <section className="p-3 w-[90%] sm:w-1/2 lg:w-1/3 xl:w-1/3 m-auto bg-amber-100 h-[85vh] rounded-lg">
+            <main className=" flex flex-col m-auto">
+              {quizAnswersQuestions[actualIndex].image != "" ? (
+                <img
+                  className="w-32 h-32 mt-10 m-auto "
+                  src={quizAnswersQuestions[actualIndex].image}
+                  alt="noImage"
+                />
+              ) : (
+                <div className="w-2 h-2 mt-2 m-auto "></div>
+              )}
+              <div className=" w-4/5 m-auto mt-5 mb-5 p-3 pb-6 bg-white rounded-md h-1/2">
+                <h2 className=" mt-5 text-center ">
+                  {quizAnswersQuestions[actualIndex].question}
+                </h2>
+              </div>
+              <ul className="m-auto flex justify-around ">
+                <li>
+                  <button
+                    onClick={(e) => {
+                      check(e, "T");
+                    }}
+                    id="true"
+                    className="bg-blue-500  text-white font-bold py-2 px-4 rounded  mr-7"
+                  >
+                    {quizAnswersQuestions[actualIndex].answers[0]}
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={(e) => {
+                      check(e, "F");
+                    }}
+                    id="false"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    {quizAnswersQuestions[actualIndex].answers[1]}
+                  </button>
+                </li>
+              </ul>
+              <button
+                onClick={changeIndex}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-16 ml-60 w-1/10 self-center flex justify-center "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
                 >
-                  {quizAnswersQuestions[actualIndex].answers[0]}
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={(e) => {
-                    check(e, "F");
-                  }}
-                  id="false"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                  {quizAnswersQuestions[actualIndex].answers[1]}
-                </button>
-              </li>
-            </ul>
-          </main>
-          <button
-            onClick={changeIndex}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            next
-          </button>{" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </button>{" "}
+            </main>
+          </section>
         </>
       )}
     </>
