871799696fd11f596b5823dc3194fd5a58e8ae20
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-18 15:47:15 +0100

    edit store + add images array

--- dinamic-gallery/src/store.js
+++ dinamic-gallery/src/store.js
@@ -1,5 +1,68 @@
-import { reactive } from "vue";
+import { createStore } from "vuex";
 
-export const store = reactive({});
-
-export default store;
+export default createStore({
+  state: {
+    images: [
+      {
+        id: 1,
+        title: "Titolo 1",
+        description: "Descrizione 1",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 2,
+        title: "Titolo 2",
+        description: "Descrizione 2",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 3,
+        title: "Titolo 3",
+        description: "Descrizione 3",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 4,
+        title: "Titolo 4",
+        description: "Descrizione 4",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 5,
+        title: "Titolo 5",
+        description: "Descrizione 5",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 6,
+        title: "Titolo 6",
+        description: "Descrizione 6",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 7,
+        title: "Titolo 7",
+        description: "Descrizione 7",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 8,
+        title: "Titolo 8",
+        description: "Descrizione 8",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 9,
+        title: "Titolo 9",
+        description: "Descrizione 9",
+        url: "https://picsum.photos/400",
+      },
+      {
+        id: 10,
+        title: "Titolo 10",
+        description: "Descrizione 10",
+        url: "https://picsum.photos/400",
+      },
+    ],
+  },
+});
